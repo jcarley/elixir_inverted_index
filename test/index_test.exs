1311d16bacb88bbc05aa65e4859ab471900a1813
jcarley <jcarley@gettyimages.com>
2016-07-15 16:45:02 -0500

    testing

--- test/index_test.exs
+++ test/index_test.exs
@@ -2,8 +2,14 @@ defmodule IndexTest do
   use ExUnit.Case
   doctest Index
 
-
   test "it is empty" do
+
+    keys = [1, 2, 3, 4, 5, 6, 7]
+    f = fn([head | tail]) ->
+      IO.inspect(head)
+      IO.inspect(tail)
+    end
+    f.(keys)
 
   end
 
